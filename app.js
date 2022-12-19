7c12d3e0cd6cc45da207c6c87f23425151865427
yong017 <zero0lemon@gmail.com>
2022-12-19 18:56:10 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
-
-const app = express()
-
+const mongoose = require('mongoose')
 const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!')
---
+++ models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  done: {
+    type: Boolean
+  }
+})
+
+module.exports = mondoose.model('Todo', todoSchema)
+//匯出的時候我們把這份 model 命名為 Todo，以後在其他的檔案直接使用 Todo 就可以操作和「待辦事項」有關的資料
