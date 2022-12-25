3849ee37af2ed74d623db2969cc01a7fc3b07502
yong017 <zero0lemon@gmail.com>
2022-12-25 16:21:49 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -67,6 +67,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.post('/todos', (req, res) => {
   const name = req.body.name
   return Todo.create({ name })
